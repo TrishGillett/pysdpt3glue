86a2f95208a1aa94f240c1ec11357ac3aeaf1eb3
Trish Gillett-Kawamoto <discardthree@gmail.com>
2016-05-14 00:52:16 -0400

    Rework setUp and tearDown.

--- unittest_neos.py
+++ unittest_neos.py
@@ -22,13 +22,16 @@ class TestSimpleNEOSSolve(unittest.TestCase):
     '''
     Testing simplification of Sedumi problems.
     '''
-    
-    def setUp(self):
-        os.mkdir('temp')
-    
-    def tearDown(self):
-        shutil.rmtree('temp')
 
+    @classmethod
+    def setUpClass(cls):
+        cls.temp_folder = "temp"
+        if not os.path.exists(cls.temp_folder):
+            os.makedirs(cls.temp_folder)
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree("temp")
 
     def test_hamming(self):
         '''
@@ -37,13 +40,15 @@ class TestSimpleNEOSSolve(unittest.TestCase):
         know the .mat isn't the problem.
         '''
         matfile_path = os.path.join('test_data', 'hamming_7_5_6.mat')
-        output_path = 'temp/hamming_out.txt'
+        output_path = os.path.join(self.temp_folder, 'hamming_out.txt')
         assert os.path.exists(matfile_path), \
             "There's nothing at the path " + matfile_path
-        result = slv.sdpt3_solve_mat(matfile_path, 'neos', output_target=output_path, discard_matfile=False)
+        result = slv.sdpt3_solve_mat(matfile_path,
+                                     'neos',
+                                     output_target=output_path,
+                                     discard_matfile=False)
 
         self.assertAlmostEqual(result['primal_z'], -42.6666661, places=2)
-
 
     def test_submission(self):
         '''
@@ -55,16 +60,14 @@ class TestSimpleNEOSSolve(unittest.TestCase):
         c = 1.*np.array([0, 1, 0, 0]).reshape(1, 4)
         K = {'l': 0., 's': [2.]}
 
-        matfile_target = os.path.join('temp', 'matfile.mat')
-        output_target = os.path.join('temp', 'output.txt')
-        
+        matfile_target = os.path.join(self.temp_folder, 'matfile.mat')
+        output_target = os.path.join(self.temp_folder, 'output.txt')
+
         sw.write_sedumi_to_mat(A, b, c, K, matfile_target)
         result = slv.sdpt3_solve_mat(matfile_target,
                                      'neos',
-                                     output_target=output_target,
-                                     discard_matfile=True)
+                                     output_target=output_target)
         res.print_summary(result)
-        
 
 
 
@@ -73,25 +76,27 @@ class TestBlackbox(unittest.TestCase):
     '''
     Test the overall process from start to finish
     '''
-    def setUp(self):
-        self.X = cvxpy.Semidef(3)
-        self.constraints = []
+    @classmethod
+    def setUpClass(cls):
+        cls.X = cvxpy.Semidef(3)
+        cls.constraints = []
 
         for i in range(3):
-            self.constraints += [self.X[i, i] == 1]
+            cls.constraints += [cls.X[i, i] == 1]
 
-        self.constraints += [-0.2 <= self.X[0, 1],
-                             self.X[0, 1] <= -0.1,
-                             0.4 <= self.X[1, 2],
-                             self.X[1, 2] <= 0.5]
-        self.temp_folder = "temp"
-        if not os.path.exists(self.temp_folder):
-            os.makedirs(self.temp_folder)
+        cls.constraints += [cls.X[0, 1] >= -0.2,
+                            cls.X[0, 1] <= -0.1,
+                            cls.X[1, 2] >= 0.4,
+                            cls.X[1, 2] <= 0.5]
+        cls.temp_folder = "temp"
+        if not os.path.exists(cls.temp_folder):
+            os.makedirs(cls.temp_folder)
 
-    @unittest.skip("")
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(cls):
         shutil.rmtree('temp')
 
+    @unittest.skip("")
     def test_min(self):
         '''
         min y s.t.
@@ -106,7 +111,10 @@ class TestBlackbox(unittest.TestCase):
 
         obj = cvxpy.Minimize(self.X[0, 2])
         problem = cvxpy.Problem(obj, self.constraints)
-        result = slv.sdpt3_solve_problem(problem, 'neos', matfile_target, output_target=output_target)
+        result = slv.sdpt3_solve_problem(problem,
+                                         'neos',
+                                         matfile_target,
+                                         output_target=output_target)
         self.assertAlmostEqual(result['primal_z'], -0.978, places=2)
 
 
@@ -125,7 +133,10 @@ class TestBlackbox(unittest.TestCase):
 
         obj = cvxpy.Maximize(self.X[0, 2])
         problem = cvxpy.Problem(obj, self.constraints)
-        result = slv.sdpt3_solve_problem(problem, 'neos', matfile_target, output_target=output_target, discard_matfile=False)
+        result = slv.sdpt3_solve_problem(problem,
+                                         'neos',
+                                         matfile_target,
+                                         output_target=output_target)
         self.assertAlmostEqual(result['primal_z'], 0.872, places=2)
 
 
