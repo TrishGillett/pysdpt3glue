9a74f11d4adfafbddec2e86251ecef17c4196bf2
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-06-10 19:49:11 -0400

    Fix a bug to return error status code when tests are failed.

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,5 +1,8 @@
 #! /usr/bin/env python
+""" Test suite.
+"""
 from __future__ import absolute_import
+import sys
 import unittest
 
 from . import unittest_neos
@@ -8,6 +11,9 @@ from . import unittest_sedumi_writer
 
 def main():
     """ The main function.
+
+    Returns:
+      True if all tests are successful.
     """
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
@@ -15,11 +21,13 @@ def main():
     suite.addTest(loader.loadTestsFromModule(unittest_neos))
     suite.addTest(loader.loadTestsFromModule(unittest_sedumi_writer))
 
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    res = unittest.TextTestRunner(verbosity=2).run(suite)
+    return res.wasSuccessful()
 
 
 if __name__ == "__main__":
     try:
-        main()
+        sys.exit(0 if main() else 1)
     except KeyboardInterrupt:
         print "Test canceled."
+        sys.exit(-1)
