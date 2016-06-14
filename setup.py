3fcdd7393ca4bc11b3aa9e93c66f393f02324049
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-06-14 02:26:53 -0400

    Add setup.py and MANIFEST.in.

---
+++ setup.py
@@ -0,0 +1,32 @@
+""" sdpt3glue package information.
+"""
+from setuptools import setup, find_packages
+
+
+def _load_requires_from_file(filepath):
+    """ Read a package list from a given file path.
+
+    Args:
+      filepath: file path of the package list.
+
+    Returns:
+      a list of package names.
+    """
+    with open(filepath) as fp:
+        return [pkg_name.strip() for pkg_name in fp.readlines()]
+
+
+setup(
+    name = 'py-sdpt3-glue',
+    version = '0.1.0',
+    description=(
+        'Glue code for solving semidefinite programs '
+        'in Cvxpy format using the SDPT3 package for Matlab.'
+    ),
+    author="Trish Gillett-Kawamoto",
+    url="https://github.com/discardthree/PySDPT3glue",
+    packages = find_packages(exclude=["tests"]),
+    install_requires=_load_requires_from_file("requirements.txt"),
+    test_suite = 'tests.suite',
+    license="MIT"
+)
