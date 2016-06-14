c64ce8c75c212c9cd93814c8ff528e1936378363
Junpei Kawamoto <kawamoto.junpei@gmail.com>
2016-06-14 00:19:42 -0400

    Update tests package to expose a test suite generator.

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -8,26 +8,33 @@ import unittest
 from . import unittest_neos
 from . import unittest_sedumi_writer
 
+def suite():
+    """ Return a test suite.
+    """
+    loader = unittest.TestLoader()
+    res = unittest.TestSuite()
+
+    res.addTest(loader.loadTestsFromModule(unittest_neos))
+    res.addTest(loader.loadTestsFromModule(unittest_sedumi_writer))
+
+    return res
+
 
 def main():
     """ The main function.
 
     Returns:
-      True if all tests are successful.
+      exit code.
     """
-    loader = unittest.TestLoader()
-    suite = unittest.TestSuite()
+    try:
+        res = unittest.TextTestRunner(verbosity=2).run(suite())
 
-    suite.addTest(loader.loadTestsFromModule(unittest_neos))
-    suite.addTest(loader.loadTestsFromModule(unittest_sedumi_writer))
+    except KeyboardInterrupt:
+        return -1
 
-    res = unittest.TextTestRunner(verbosity=2).run(suite)
-    return res.wasSuccessful()
+    else:
+        return 0 if res.wasSuccessful() else 1
 
 
 if __name__ == "__main__":
-    try:
-        sys.exit(0 if main() else 1)
-    except KeyboardInterrupt:
-        print "Test canceled."
-        sys.exit(-1)
+    sys.exit(main())
